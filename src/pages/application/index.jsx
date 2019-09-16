b67bc8a04ad25f7002d82153b7f36427c5347c84
chenmmx <1107204891@qq.com>
2019-09-16 11:37:18 +0800

    Modify some place of application

--- src/pages/application/index.jsx
+++ src/pages/application/index.jsx
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import FsTitle from '../../components/common/fs-title';
 
 class Application extends Component {
   constructor(props) {
     super(props);
-    this.state = { };
+    this.state = {
+    //   dataList: []
+    };
   }
 
+
   render() {
+    // const { dataList } = this.state;
     return (
-      <h1>Application</h1>
+      <div id="application">
+        <FsTitle title="应用管理" />
+        <div className="application-content">
+          <div className="application-content-create">
+            <div className="application-content-create--title">您还没有创建应用，立即开始</div>
+            <div className="application-content-create--add">1232</div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
