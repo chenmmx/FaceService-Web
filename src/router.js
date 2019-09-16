2286d2fe4b96b28bbb876db6a4c6b5f3719596c2
chenmmx <1107204891@qq.com>
2019-09-16 15:37:45 +0800

    Disbaled redirect router

--- src/router.js
+++ src/router.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Application from './pages/application';
+import ApplicationCreateForm from './pages/application/form/createForm';
 import Callback from './pages/callback';
 import Device from './pages/device';
 import Person from './pages/person';
@@ -11,7 +12,10 @@ import Record from './pages/record';
 export default function AppRouter() {
   const routeConfig = [
     {
-      id: '0', path: '/application', title: '应用管理', exact: false, component: Application
+      id: '0', path: '/application', title: '应用管理', exact: true, component: Application
+    },
+    {
+      id: '5', path: '/application/create', title: '应用管理', exact: false, component: ApplicationCreateForm
     },
     {
       id: '1', path: '/callback', title: '回调管理', exact: false, component: Callback
@@ -30,7 +34,6 @@ export default function AppRouter() {
   return (
   // <Router>
     <div className="routes">
-      <Redirect to="/application" />
       {
         routeConfig.map((item) => (
           <Route
