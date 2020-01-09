9efc8c1d3df9c9b483f77a784264410f23735259
chenmmx <1107204891@qq.com>
2020-01-09 10:57:02 +0800

    delete eslint error in setup proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,6 @@
-/* eslint-disable func-names */
 const proxy = require('http-proxy-middleware');
 
-module.exports = function (app) {
+module.exports = (app) => {
   app.use(
     proxy('/api', {
       target: 'http://192.168.1.236:5000',
